d1e3cc156429645189b9bee7689b2ddb692aea9b
HalleXqC <superanvar11@gmail.com>
2023-02-22 19:03:22 +0600

    attempt to deploy 1

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,   
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-    }
-)
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,   
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//     }
+// )
+
+module.exports = new Sequelize('postgres://scintilla:J7jURiU8Ku8va8iedn3zQBIayBC6UImV@dpg-cfr133hgp3joa8hgjel0-a/todo_xcia')
